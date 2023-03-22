2235cd5c4f71eff373a4670318df7a292221f01f
Matt Jones <67526111+Matt-J-Jones@users.noreply.github.com>
2023-03-22 17:09:11 +0000

    Added Unit and Integration Tests

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,26 +1,64 @@
+require 'twilio-ruby'
+
+
 class TakeawayOrder
-  def initialize 
+  def initialize(menu, io = Kernel)
     @order = Array.new
+    @io = io
+    @menu = menu
   end
 
   def show_menu
+    @menu.each_with_index { |item, index|
+      @io.puts "#{index+1}. #{item.return_item}"
+    }
+    # menu is return value from class Menu
     # Shows the menu, prices, and avalibility
   end
 
-  def order_items
+  def add_to_order(item)
+
+    if item.availability?
+      @order << item
+      @io.puts "Item Added: #{item.return_name}"
+    else
+      @io.puts "Item Sold Out"
+    end
     # Prompts user to choose items from menu
     # Checks avalibility, and adds to order if avalible
     # Confirms item ordered
   end
 
+  def return_order
+    return @order
+  end
+
   def confirm_order
+    total_price = 0
+    @order.each_with_index { |item, index|
+      @io.puts "#{index+1}. #{item.return_item}"
+      total_price += item.return_price
+    }
+    @io.puts "Total: £#{sprintf('%.2f', total_price)}"
+
     # Prints itemised list of all items ordered
     # Shows total price
     # Prompts user to confirm and continue
   end
 
-  def tracking_and_confirmation
-    # Confirms order
-    # Sends message with estimated delivery time
+  def tracking_and_confirmation(time=(Time.now+900), client=Twilio::REST::Client.new(ENV['SID'], ENV['AUTH']))
+
+    body = "Your order will arrive at #{time.hour}:#{time.min}"
+    
+    client.messages.create(
+      from: '+14346867589',
+      to: ENV['PHONE'],
+      body: body
+    )
+    return "I was successful"
   end
 end
+
+# takeaway_order = TakeawayOrder.new
+# items = ["Chips: £2.00, Available", 2.0],["Rice: £3.00, Sold Out", 3.00]
+# takeaway_order.show_menu(items)
---
+++ spec/takeaway_spec.rb
@@ -0,0 +1,82 @@
+require 'takeaway'
+
+RSpec.describe TakeawayOrder do
+
+  let(:fake_rice) { double(:fake_rice, name:"Rice", price: 3.00, availability: false) }
+  let(:fake_chips) { double(:fake_chips, name:"Chips", price: 2.00, availability: true) }
+  let(:io) { double :io }
+  
+  it "creates new order, returns menu" do    
+    menu = [fake_rice, fake_chips]
+    order = TakeawayOrder.new(menu, io)
+    allow(fake_rice).to receive(:return_item).and_return("Rice: £3.00, Sold Out")
+    allow(fake_chips).to receive(:return_item).and_return("Chips: £2.00, Available")
+    expect(io).to receive(:puts).with("1. Rice: £3.00, Sold Out")
+    expect(io).to receive(:puts).with("2. Chips: £2.00, Available")
+    order.show_menu
+  end
+
+  it "creates new order, returns blank order" do
+    menu = []
+    order = TakeawayOrder.new(menu)
+    expect(order.return_order).to eq []
+  end
+
+  it "creats new order, adds item to basket, returns" do
+    allow(fake_rice).to receive(:availability?).and_return(false)
+    allow(fake_chips).to receive(:availability?).and_return(true)
+    allow(fake_chips).to receive(:return_name).and_return("Chips")
+    menu = [fake_rice, fake_chips]
+    order = TakeawayOrder.new(menu, io)
+    expect(io).to receive(:puts).with("Item Sold Out")
+    order.add_to_order(fake_rice)
+
+    expect(io).to receive(:puts).with("Item Added: Chips")
+    order.add_to_order(fake_chips)
+  end
+
+  it "creates new order, adds items to order, returns order" do
+    allow(fake_rice).to receive(:availability?).and_return(false)
+    allow(fake_chips).to receive(:availability?).and_return(true)
+    allow(fake_chips).to receive(:return_name).and_return("Chips")
+    menu = [fake_rice, fake_chips]
+    order = TakeawayOrder.new(menu, io)
+    expect(io).to receive(:puts).with("Item Added: Chips")
+    order.add_to_order(fake_chips)
+    expect(order.return_order).to eq [fake_chips]
+  end
+  
+  it "creates new order, adds items, returns order total and price" do
+    allow(fake_rice).to receive(:availability?).and_return(false)
+    allow(fake_chips).to receive(:availability?).and_return(true)
+    allow(fake_rice).to receive(:return_item).and_return("Rice: £3.00, Sold Out")
+    allow(fake_chips).to receive(:return_item).and_return("Chips: £2.00, Available")
+    allow(fake_chips).to receive(:return_name).and_return("Chips")
+    allow(fake_chips).to receive(:return_price).and_return(2.00)
+
+    menu = [fake_rice, fake_chips]
+    order = TakeawayOrder.new(menu, io)
+
+    expect(io).to receive(:puts).with("Item Added: Chips")
+    expect(io).to receive(:puts).with("Item Added: Chips")
+    order.add_to_order(fake_chips)
+    order.add_to_order(fake_chips)
+
+    
+    expect(io).to receive(:puts).with("1. Chips: £2.00, Available")
+    expect(io).to receive(:puts).with("2. Chips: £2.00, Available")
+    
+    expect(io).to receive(:puts).with("Total: £4.00")
+    order.confirm_order
+  end
+
+  it "Receives order, returns delivery time" do
+    menu = [fake_rice, fake_chips]
+    order = TakeawayOrder.new(menu, io)
+    fake_time = Time.new(2023, 3, 22, 16, 45, 25)
+    fake_client = double :client
+    allow(fake_client).to receive_message_chain(:messages, :create).with(from: '+14346867589', to: ENV['PHONE'], body: "Your order will arrive at 16:45")
+    expect(order.tracking_and_confirmation(fake_time, fake_client)).to eq "I was successful"
+    
+  end
+end
